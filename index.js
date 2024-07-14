777271ddf3e4e3e54720acb1ca9d9518bc949436
Paingkhant0397 <paingkhant0397@gmail.com>
2024-07-14 12:44:11 +0630

    updated PORT

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ const unknownEndpoints = (req, res) => {
 app.use(unknownEndpoints)
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`)
 })
