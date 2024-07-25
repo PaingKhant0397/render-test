52291eb1e59f86005cded633f2645ae940e6e930
PaingKhant0397 <116296836+PaingKhant0397@users.noreply.github.com>
2024-07-25 14:44:27 +0630

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const express = require('express');
 const mongoose = require('mongoose')
 const Note = require('./models/note')
-
+console.log('sdf')
 const app = express()
 app.use(express.json());
 
